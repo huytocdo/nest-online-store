9d18c262f7ab56b73203b78533bc66956f76d2c3
huytocdo <ndhuyspkt@gmail.com>
2023-05-14 20:54:31 +0700

    end of chapter 20

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -7,6 +7,7 @@ import {
   Render,
   UseInterceptors,
   UploadedFile,
+  Param,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductsService } from '../models/products.service';
@@ -25,6 +26,41 @@ export class AdminProductsController {
     return {
       viewData,
     };
+  }
+
+  @Post('/:id')
+  @Redirect('/admin/products')
+  remove(@Param('id') id: string) {
+    return this.productService.remove(id);
+  }
+
+  @Get('/:id')
+  @Render('admin/products/edit')
+  async edit(@Param('id') id: string) {
+    const viewData = {};
+    viewData['title'] = 'Admin Page - Edit Product - Online Store';
+    viewData['product'] = await this.productService.findOne(Number(id));
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Post('/:id/update')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
+  @Redirect('/admin/products')
+  async update(
+    @Body() body,
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id: string,
+  ) {
+    const product = await this.productService.findOne(Number(id));
+    product.setName(body.name);
+    product.setDescription(body.description);
+    product.setPrice(body.price);
+    if (file) {
+      product.setImage(file.filename);
+    }
+    await this.productService.createOrUpdate(product);
   }
 
   @Post('/store')
